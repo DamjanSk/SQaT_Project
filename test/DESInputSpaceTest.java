d3e48da514230b257e07ee0c11beb5cb5bff4454
Damjan <dame.najdov@gmail.com>
2018-08-16 15:08:46 +0200

    Test environment setup.

---
+++ test/DESInputSpaceTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DESInputSpaceTest {
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void encrypt1() {
+    }
+
+    @Test
+    void decrypt() {
+    }
+}
