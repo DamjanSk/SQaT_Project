0b3f3319b12a0f5760aaae54d4e0f6125ef5eda9
Hristijan Draganovski <hristijan.draganovski@students.finki.ukim.mk>
2018-08-18 14:19:51 +0200

    shiftAccuracy fix

--- test/DESInputSpaceTest.java
+++ test/DESInputSpaceTest.java
@@ -1,11 +1,21 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+class ArrayWrapper{
+    public int[] arr;
+
+    public ArrayWrapper(int[] arr) {
+        this.arr = arr;
+    }
+}
 
 class DESInputSpaceTest {
 
@@ -32,7 +42,32 @@ class DESInputSpaceTest {
     // - Relation to size of array: <size, size, >size
 
     // Tests for all combination coverage except for nulls.
-    private static Object[][] shiftTestValues() {
+
+    private static Stream<Arguments> shiftTestValues() {
+        return Stream.of(
+                                //array                                                     amount,  expected result
+                Arguments.of(   new ArrayWrapper(new int[]{}),                              -1,      new ArrayWrapper(new int[]{})),
+                Arguments.of(   new ArrayWrapper(new int[]{}),                               0,      new ArrayWrapper(new int[]{})),
+                Arguments.of(   new ArrayWrapper(new int[]{}),                               1,      new ArrayWrapper(new int[]{})),
+                Arguments.of(   new ArrayWrapper(new int[]{}),                              -5,      new ArrayWrapper(new int[]{})),
+                Arguments.of(   new ArrayWrapper(new int[]{}),                               0,      new ArrayWrapper(new int[]{})),
+                Arguments.of(   new ArrayWrapper(new int[]{}),                              50,      new ArrayWrapper(new int[]{})),
+
+                Arguments.of(   new ArrayWrapper(new int[]{5}),                             -1,      new ArrayWrapper(new int[]{5})),
+                Arguments.of(   new ArrayWrapper(new int[]{3}),                              0,      new ArrayWrapper(new int[]{3})),
+                Arguments.of(   new ArrayWrapper(new int[]{-1}),                             1,      new ArrayWrapper(new int[]{-1})),
+                Arguments.of(   new ArrayWrapper(new int[]{9}),                             -5,      new ArrayWrapper(new int[]{9})),
+                Arguments.of(   new ArrayWrapper(new int[]{-5}),                             1,      new ArrayWrapper(new int[]{-5})),
+                Arguments.of(   new ArrayWrapper(new int[]{0}),                             50,      new ArrayWrapper(new int[]{0})),
+
+                Arguments.of(   new ArrayWrapper(new int[]{1, 2, 3}),                       -1,      new ArrayWrapper(new int[]{3, 1, 2})),
+                Arguments.of(   new ArrayWrapper(new int[]{2, 2, 2, 2}),                     0,      new ArrayWrapper(new int[]{2, 2, 2, 2})),
+                Arguments.of(   new ArrayWrapper(new int[]{-5, 0, 5}),                       1,      new ArrayWrapper(new int[]{0, 5, -5})),
+                Arguments.of(   new ArrayWrapper(new int[]{1, 2}),                          -5,      new ArrayWrapper(new int[]{2, 1})),
+                Arguments.of(   new ArrayWrapper(new int[]{10, 20, 30, 40, 50, 60}),         6,      new ArrayWrapper(new int[]{10, 20, 30, 40, 50, 60})),
+                Arguments.of(   new ArrayWrapper(new int[]{1, 1, 2, 3, 5, 8, 13}),          50,      new ArrayWrapper(new int[]{1, 2, 3, 5, 8, 13, 1}))
+        );
+        /*
         return new Object[][]{
                 // array,                              amount,    expected result
                 {new int[]{},                              -1,    new int[]{}},
@@ -56,12 +91,13 @@ class DESInputSpaceTest {
                 {new int[]{10, 20, 30, 40, 50, 60},         6,    new int[]{10, 20, 30, 40, 50, 60}},
                 {new int[]{1, 1, 2, 3, 5, 8, 13},          50,    new int[]{1, 2, 3, 5, 8, 13, 1}}
         };
+        */
     }
 
     @ParameterizedTest
     @MethodSource("shiftTestValues")
-    void shiftAccuracy(int[] array, int amount, int[] expected) {
-        assertArrayEquals(expected, des.shift(array, amount));
+    void shiftAccuracy(ArrayWrapper array, int amount, ArrayWrapper expected) {
+        assertArrayEquals(expected.arr, des.shift(array.arr, amount));
     }
 
     @Test
